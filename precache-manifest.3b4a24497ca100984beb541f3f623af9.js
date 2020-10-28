668fc308f93e6327a0e845431c12ff096359a080
lucky4030 <lucky@.github.com>
2020-10-28 15:30:53 +0530

    Updates

---
+++ precache-manifest.3b4a24497ca100984beb541f3f623af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b276151a83eaba5d89906d24653358c",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "2e00ef30f38ac4f32b1d",
+    "url": "/todoApp/static/js/2.22c959d0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/todoApp/static/js/2.22c959d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bf49c4bfc51907e892",
+    "url": "/todoApp/static/js/main.b0f69fc3.chunk.js"
+  },
+  {
+    "revision": "d4ecc523e189f0b990aa",
+    "url": "/todoApp/static/js/runtime-main.51868266.js"
+  }
+]);
